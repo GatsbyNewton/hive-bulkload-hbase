08ef8057d66beaa69a180a90c999858faaafe339
GatsbyNewton <wangzmking@163.com>
2016-04-03 23:20:30 +0800

    start script

--- bin/run.sh
+++ bin/run.sh
@@ -1,14 +1,19 @@
 #!/usr/bin/env bash
 
-#Transform RCFile into HFile.
+# Transform RCFile into HFile.
+# param:
+# 	RCFile's input path
+#	generated HFile's path
+#	schema file's local path
+#	hbase table's name
 hadoop jar hfile-1.0-SNAPSHOT-jar-with-dependencies.jar edu.wzm.Driver  \
 /rcfile/test    \
 /hfile/hbase  \
 /schema.hql   \
 hbase_table
 
-#Distributed copy HFile to mycluster-hbase.
+# Distributed copy HFile to mycluster-hbase.
 hadoop distcp hdfs://mycluster-hive/hfile/hbase hdfs://mycluster-hbase/hbase/test
 
-#BulkLoad HFile into hbase table on mycluster-hbase.
+# BulkLoad HFile into hbase table on mycluster-hbase.
 hbase org.apache.hadoop.hbase.mapreduce.LoadIncrementalHFiles /hbase/test hbase_table
